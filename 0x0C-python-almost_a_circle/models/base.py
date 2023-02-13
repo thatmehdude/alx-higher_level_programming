e4169d0cbbef49e6521ee80b2e5aa13e5bbe60d3
thatmehdude <104757542+thatmehdude@users.noreply.github.com>
2023-02-13 10:49:32 +0100

    updated Base class

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -2,5 +2,13 @@
 
 import unittest
 
+
 class Base:
-    pass
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects

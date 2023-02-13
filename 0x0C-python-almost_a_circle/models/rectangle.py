4859fb077dfdc2cfcd9e0634437bca4f6820df80
thatmehdude <104757542+thatmehdude@users.noreply.github.com>
2023-02-13 12:35:04 +0100

    added setter for x and y

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -22,6 +22,7 @@ class Rectangle(Base):
             raise TypeError("width must be an integer")
         if value <= 0:
             raise ValueError("width must be > 0")
+        self.__width = value
 
     @property
     def height(self):
@@ -34,4 +35,33 @@ class Rectangle(Base):
             raise TypeError("height must be an integer")
         if value <= 0:
             raise ValueError("height must be > 0")
+        self.__height = value
+
+    @property
+    def x(self):
+        """returns value of x"""
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        """sets value of x"""
+        if not isinstance(value, int):
+            raise TypeError("x must be an integer")
+        if value < 0:
+            raise ValueError("x must be >= 0")
+        self.__x = value
+
+    @property
+    def y(self):
+        """returns value of y"""
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        """sets value of y"""
+        if not isinstance(value, int):
+            raise TypeError("y must be an integer")
+        if value < 0:
+            raise ValueError("y must be >= 0")
+        self.__y = value
 
